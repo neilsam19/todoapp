941fc5086bce669908b4fb1c46cb7eeaf64fb89d
Neil Samant <nsamant8@gatech.edu>
2024-11-08 00:36:40 -0500

    Adding and Saving state of todos

--- todolist/src/components/Todo.jsx
+++ todolist/src/components/Todo.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTodo(e.target.value)}
           value={todo}
@@ -12,6 +18,7 @@ export default function Todo() {
         />
         <button type="submit">Add</button>
       </form>
+      {console.log(todos)}
     </div>
   );
 }
