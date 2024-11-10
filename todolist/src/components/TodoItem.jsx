d61c30cea54a511d94d30ae4d2cc19097ef1e1fd
Neil Samant <nsamant8@gatech.edu>
2024-11-10 16:08:05 -0500

    Added delete functionality

--- todolist/src/components/TodoItem.jsx
+++ todolist/src/components/TodoItem.jsx
@@ -1,9 +1,24 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
