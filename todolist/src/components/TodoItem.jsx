b195455f54f662286689ceac579f2a538fc1da8c
Neil Samant <nsamant8@gatech.edu>
2024-11-10 16:27:30 -0500

    Adding mark-as-done functionality

--- todolist/src/components/TodoItem.jsx
+++ todolist/src/components/TodoItem.jsx
@@ -4,11 +4,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
-
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+  const className = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
